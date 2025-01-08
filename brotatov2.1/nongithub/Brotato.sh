dd1a405a8c3726bc0e37eaf3003e646c483852a7
Damian2809 <lothk113@gmail.com>
2025-01-08 15:29:07 +0100

    Brotato v2.1 nongithub

---
+++ brotatov2.1/nongithub/Brotato.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
+
+if [ -d "/opt/system/Tools/PortMaster/" ]; then
+  controlfolder="/opt/system/Tools/PortMaster"
+elif [ -d "/opt/tools/PortMaster/" ]; then
+  controlfolder="/opt/tools/PortMaster"
+elif [ -d "$XDG_DATA_HOME/PortMaster/" ]; then
+  controlfolder="$XDG_DATA_HOME/PortMaster"
+else
+  controlfolder="/roms/ports/PortMaster"
+fi
+
+source $controlfolder/control.txt
+
+[ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
+
+get_controls
+
+GAMEDIR=/$directory/ports/brotato/
+CONFDIR="$GAMEDIR/conf/"
+
+# Ensure the conf directory exists
+mkdir -p "$GAMEDIR/conf"
+
+# Set the XDG environment variables for config & savefiles
+export XDG_CONFIG_HOME="$CONFDIR"
+export XDG_DATA_HOME="$CONFDIR"
+export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
+# KDOGs Hacksdl ( You are a god )
+LD_PRELOAD="$GAMEDIR/hacksdl/hacksdl.aarch64.so" HACKSDL_DEVICE_DISABLE_0=2
+
+cd $GAMEDIR
+> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
+
+export FRT_NO_EXIT_SHORTCUTS=FRT_NO_EXIT_SHORTCUTS
+
+[ -f "./gamedata/Brotato.pck" ] && mv gamedata/Brotato.pck gamedata/brotato.pck
+
+$GPTOKEYB "brotato_runner" -c "./brotato.gptk" &
+pm_platform_helper "$GAMEDIR/brotato_runner"
+$GAMEDIR/brotato_runner $GODOT_OPTS --main-pack "gamedata/brotato.pck"
+
+$ESUDO umount "$godot_dir"
+pm_finish
---
+++ brotatov2.1/nongithub/brotato/brotato.md
@@ -0,0 +1,16 @@
+## Notes
+
+The DLC from Steam are not compatible with the Epic version of the game that is used in this port.
+Brotato wont run good on anything lower than a rk3588
+
+Thanks to [Blobfish Games](https://www.blobfish.dev/) for making this wonderful roguelite
+Thanks To [Kdog](https://github.com/cdeletre/hacksdl) for his work on HackSDL as this made the port possible
+And Thanks to you for playing it
+
+## Controls
+
+| Button | Action |
+|--|--| 
+|DPad/Lstick|Movement|
+
+
